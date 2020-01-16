e409882c6c8fc70e8c3f5a742bb693b744bac9be
jerichofs <kiberonex@protonmail.com>
2020-01-16 17:49:28 +0200

    Client: Minor changes in webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,11 +4,10 @@ const SRC_PATH = path.join(__dirname, "/src");
 const APP_PATH = path.join(SRC_PATH, "/index.jsx");
 const COMPONENTS_PATH = path.join(SRC_PATH, "/components");
 
-console.log("APP_PATH", APP_PATH);
 // plugins
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// "webpack-dev-server/client?http://localhost:3000",
+
 module.exports = {
   mode: "development",
 
